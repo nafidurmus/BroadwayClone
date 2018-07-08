b442513b81379afe7bf6b3f274f5f078ee1dc6d2
nafidurmus <nafidurmus07@gmail.com>
2018-07-08 16:35:13 +0300

    Add Dropdown and Fix Edit Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require bootstrap-sprockets
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
